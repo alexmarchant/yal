02424308deea878afb7e6c5ca0cbe3f2a4e280f8
Alex Marchant <alexjmarchant@gmail.com>
2021-02-20 12:45:13 -0500

    First

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'standard'
+  ],
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    'no-console': 'off',
+    "no-useless-escape": 0,
+    "no-debugger": 1,
+    "no-unused-vars": 0,
+    "indent": "off",
+    "@typescript-eslint/indent": ["error", 2]
+  },
+  overrides: [{
+    files: [
+      '**/tests/**/*.{j,t}s?(x)'
+    ],
+    env: {
+      jest: true
+    }
+  }]
+}
